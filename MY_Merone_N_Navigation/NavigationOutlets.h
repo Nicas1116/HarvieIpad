fbbfb4d1a68cdd58129976ce3e87c5d3bb6bed2b
Nicas <weiheng.lee@publicis.com.my>
2013-05-31 12:47:15 +0800

    MY_Merone_N_Navigation

---
+++ MY_Merone_N_Navigation/NavigationOutlets.h
@@ -0,0 +1,14 @@
+//
+//  NavigationOutlets.h
+//  MY_MeronemTestEx
+//
+//  Created by chan yin lu on 5/31/13.
+//  Copyright (c) 2013 Jason Allum. All rights reserved.
+//
+
+#ifndef MY_MeronemTestEx_NavigationOutlets_h
+#define MY_MeronemTestEx_NavigationOutlets_h
+
+
+
+#endif
